3cdef9286f83fc9f68322946fd98f240fb5a682e
daniela moreno <danip2824@gmail.com>
2022-04-01 08:11:33 -0300

    mejorando el ejercicio

--- ejercicio02.js
+++ ejercicio02.js
@@ -1,12 +1,25 @@
 
-const nums = [1,2,4,5]
+const nums = [1,2,4,4]
 
 let requiredSum = 8
 
-const sum = nums.map((valor,indice)=> valor + indice)
-console.log(sum);
 
-const resultado = sum.some((valor) => valor === requiredSum)
+function operacionUno () {
+for(i=0; i< nums.length; i ++)
+if(nums[0] + nums[1] === requiredSum){
+    return true 
+}else
+    return false
+}
 
-console.log(resultado);
+console.log(operacionUno());
 
+function operacionDos () {
+    for(i=0; i< nums.length; i ++)
+    if(nums[2] + nums[3] === requiredSum){
+    return true 
+    }else
+    return false
+    }
+
+console.log(operacionDos());
