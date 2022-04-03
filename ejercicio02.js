fa2295a52cb150d2e8bc670f319d4c6f9ebc836f
daniela moreno <danip2824@gmail.com>
2022-04-03 18:41:01 -0300

    probando

--- ejercicio02.js
+++ ejercicio02.js
@@ -3,7 +3,11 @@ const nums = [1,2,4,4]
 
 let requiredSum = 8
 
+const filtrado = nums.filter(function(valor){
+    return valor ===4
+})
 
+/*
 function operacionUno () {
 for(i=0; i< nums.length; i ++)
 if(nums[0] + nums[1] === requiredSum){
@@ -14,9 +18,11 @@ if(nums[0] + nums[1] === requiredSum){
 
 console.log(operacionUno());
 
+*/
+
 function operacionDos () {
-    for(i=0; i< nums.length; i ++)
-    if(nums[2] + nums[3] === requiredSum){
+    for(i=0; i< filtrado.length; i ++)
+    if(filtrado[0] + filtrado[1] === requiredSum){
     return true 
     }else
     return false
